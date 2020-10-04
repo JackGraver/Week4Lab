acc7c0e817435ddd2ebe6cdc461ba2be385569a8
815138 <815138@SAIT224414IT.ACDM.DS.SAIT.CA>
2020-10-04 16:57:08 -0600

    Finished working copy of lab.

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -13,8 +13,33 @@ import java.io.Serializable;
  */
 public class Note implements Serializable {
     
+    private String title;
+    private String contents;
+    
     public Note() {
         
+    }
+    
+    public Note(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    
+    public String getContents() {
+        return contents;
     }
     
 }
--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,27 +1,69 @@
 package servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Note;
 
 /**
  *
  * @author 815138
  */
 public class NoteServlet extends HttpServlet {
-
+    
+      
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+        
+     
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        
+         
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        
+
+        
+        Note note = new Note(br.readLine(), br.readLine());
+        
+        request.setAttribute("note", note);
+        
+        br.close();
+        
+        if(request.getParameter("edit") != null && request.getParameter("edit").equals("")) {
+             getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        }
+        
+        
+        
     }
 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- 
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+          
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
+        
+        pw.println(request.getParameter("title"));
+                
+        pw.println(request.getParameter("contents"));
+        
+        pw.close();
+        
+        
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+          
+        Note note = new Note(br.readLine(), br.readLine());
+        
+        br.close();
+        
+        request.setAttribute("note", note);
+        
+         
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
     }
 }
